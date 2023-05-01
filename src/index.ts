a2eabee48cdede3c6008770f37167c40304c3433
Stan van Oers <stan@monwest.nl>
2023-05-01 22:36:24 +1000

    Removed intialize.

--- src/index.ts
+++ src/index.ts
@@ -4,56 +4,52 @@ import { Parser } from "./Parser";
 import template from "./template";
 import config from "./config";
 
-const initialize = () => {
+/**
+ *
+ * Initialise erase.js with the
+ * default values.
+ */
+const erase = new Erase({
+  context: document.body,
+  selector: "erase",
+  ignore: "erase-ignore",
+});
+
+/**
+ *
+ * Only show banner when
+ * data-erase attribute is
+ * ont the page
+ */
+if (erase.hasElements) {
   /**
    *
-   * Initialise erase.js with the
-   * default values.
+   * Create MOUNT element and
+   * appends it to the body.
    */
-  const erase = new Erase({
-    context: document.body,
-    selector: "erase",
-    ignore: "erase-ignore",
-  });
+  const MOUNT = document.createElement("div");
+  document.body.appendChild(MOUNT);
 
   /**
    *
-   * Only show banner when
-   * data-erase attribute is
-   * ont the page
+   * Initialize HTML parser, template
+   * function is rendered to HTML and
+   * injected in the MOUNT element.
    */
-  if (erase.hasElements) {
-    /**
-     *
-     * Create MOUNT element and
-     * appends it to the body.
-     */
-    const MOUNT = document.createElement("div");
-    document.body.appendChild(MOUNT);
+  const parser = new Parser({ element: MOUNT, template: template });
+  parser.render(config);
 
-    /**
-     *
-     * Initialize HTML parser, template
-     * function is rendered to HTML and
-     * injected in the MOUNT element.
-     */
-    const parser = new Parser({ element: MOUNT, template: template });
-    parser.render(config);
+  /**
+   *
+   * Set initial state.
+   */
+  erase.ligeratures = parser.elements.checkbox.checked;
 
-    /**
-     *
-     * Set initial state.
-     */
+  /**
+   *
+   * Set state on checkbox change.
+   */
+  parser.elements.checkbox.addEventListener("change", () => {
     erase.ligeratures = parser.elements.checkbox.checked;
-
-    /**
-     *
-     * Set state on checkbox change.
-     */
-    parser.elements.checkbox.addEventListener("change", () => {
-      erase.ligeratures = parser.elements.checkbox.checked;
-    });
-  }
-};
-
-window.addEventListener("load", initialize);
+  });
+}
